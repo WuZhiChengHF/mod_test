a3b6afb089cfb446f10a59dc82009cd5d23d254d
root <936288073@qq.com>
2018-08-13 17:50:28 +0800

    添加list pool

--- headers/modsecurity/variable_value.h
+++ headers/modsecurity/variable_value.h
@@ -38,115 +38,175 @@ void* operator new[](size_t size);
 void  operator delete(void* p);
 void  operator delete[](void* p);
 
-namespace modsecurity {
+namespace modsecurity
+{
 
 #include<list>
 using std::list;
 template<typename T>
 class CacheList
 {
-    protected:
-        list<T*> clist;
-    public:
-        CacheList(){}
-        ~CacheList()
+protected:
+    list<T*> clist;
+public:
+    CacheList() {}
+    ~CacheList()
+    {
+        while (!clist.empty())
         {
-            while (!clist.empty())
-            {
-                T* p = clist.front();
-                delete p;
-                clist.pop_front();
-            }
-        }
-
-        T* pop_one(const T* o)
-        {
-            if (clist.size() == 0)
-            {
-                return (T*) new T(o); 
-            }
             T* p = clist.front();
-            p->init(o);
+            delete p;
             clist.pop_front();
-            return p;
         }
+    }
 
-        T* pop_one(void);
-
-        T* push_one(T* t)
+    T* pop_one(const T* o)
+    {
+        if (clist.size() == 0)
         {
-            t->clear();
-           clist.push_back(t);
+            return (T*) new T(o);
         }
+        T* p = clist.front();
+        p->init(o);
+        clist.pop_front();
+        return p;
+    }
 
-        int get_conut()
-        {
-            return clist.size(); 
-        }
+    T* pop_one(void);
+
+    T* push_one(T* t)
+    {
+        t->clear();
+        clist.push_back(t);
+    }
+
+    int get_conut()
+    {
+        return clist.size();
+    }
 };
 
 class Collection;
-class VariableValue {
- public:
+class VariableValue
+{
+public:
     explicit VariableValue(const std::string *key) :
         m_key(""),
-        m_value("") {
-            m_key.assign(*key);
-            m_keyWithCollection = std::make_shared<std::string>(*key);
-        }
+        m_value("")
+    {
+        m_key.assign(*key);
+        m_keyWithCollection = std::make_shared<std::string>(*key);
+        m_from_pool = false;
+    }
 
     VariableValue(const std::string *key, const std::string *value) :
         m_key(""),
-        m_value("") {
-            m_key.assign(*key);
-            m_value.assign(*value);
-            m_keyWithCollection = std::make_shared<std::string>(*key);
-        }
+        m_value("")
+    {
+        m_key.assign(*key);
+        m_value.assign(*value);
+        m_keyWithCollection = std::make_shared<std::string>(*key);
+        m_from_pool = false;
+    }
 
     VariableValue() :
         m_key(""),
-        m_value("") {
-            m_keyWithCollection = std::make_shared<std::string>(m_key);
-        }
+        m_value("")
+    {
+        m_keyWithCollection = std::make_shared<std::string>(m_key);
+        m_from_pool = false;
+    }
 
     VariableValue(const std::string *a, const std::string *b, const std::string *c) :
         m_key(*a + ":" + *b),
-        m_value(*c) {
-            m_keyWithCollection = std::make_shared<std::string>(*a + ":" + *b);
-        }
+        m_value(*c)
+    {
+        m_keyWithCollection = std::make_shared<std::string>(*a + ":" + *b);
+        m_from_pool = false;
+    }
 
     VariableValue(std::shared_ptr<std::string> fullName) :
         m_key(""),
-        m_value("") {
-            m_keyWithCollection = fullName;
-            m_key.assign(*fullName.get());
-        }
+        m_value("")
+    {
+        m_keyWithCollection = fullName;
+        m_key.assign(*fullName.get());
+        m_from_pool = false;
+    }
 
     VariableValue(std::shared_ptr<std::string> fullName, const std::string *value) :
         m_key(""),
-        m_value("") {
-            m_value.assign(*value);
-            m_keyWithCollection = fullName;
-            m_key.assign(*fullName.get());
-        }
+        m_value("")
+    {
+        m_value.assign(*value);
+        m_keyWithCollection = fullName;
+        m_key.assign(*fullName.get());
+        m_from_pool = false;
+    }
 
 
     explicit VariableValue(const VariableValue *o) :
         m_key(""),
-        m_value("") {
+        m_value("")
+    {
         m_key.assign(o->m_key);
         m_value.assign(o->m_value);
         m_col.assign(o->m_col);
         m_keyWithCollection = o->m_keyWithCollection;
 
-        for (auto &i : o->m_orign) {
+        for (auto &i : o->m_orign)
+        {
             std::unique_ptr<VariableOrigin> origin(new VariableOrigin());
             origin->m_offset = i->m_offset;
             origin->m_length = i->m_length;
             m_orign.push_back(std::move(origin));
         }
+        m_from_pool = false;
     }
 
+    inline void init(const VariableValue* o)
+    {
+        m_key.assign(o->m_key);
+        m_value.assign(o->m_value);
+        m_col.assign(o->m_col);
+        m_keyWithCollection = o->m_keyWithCollection;
+
+        for (auto &i:o->m_orign)
+        {
+            VariableOrigin* p = clist_value_orig.pop_one();
+            std::unique_ptr<VariableOrigin> origin(p);
+            origin->m_offset = i->m_offset;
+            origin->m_length = i->m_length;
+            m_orign.push_back(std::move(origin));
+        }
+
+        m_from_pool = true;
+    }
+    
+    inline void clear()
+    {
+        for (auto &i: this->m_orign)
+        {
+            clist_value_orig.push_one(i.release());
+        }
+    }
+
+    static bool free_value(const VariableValue* v)
+    {
+        if (v == 0) return false;
+
+        if (v->m_from_pool)
+        {
+            clist_value.push_one((VariableValue*)v);
+        }
+        else delete v;
+
+        return true;
+    }
+
+    static CacheList<VariableValue> clist_value;
+    static CacheList<VariableOrigin> clist_value_orig;
+    bool m_from_pool;
     std::string m_key;
     std::string m_value;
     std::string m_col;
